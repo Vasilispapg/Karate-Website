1af7c71672bc59853d563f7b97f3987b2daf0bbb
Papagrigoriou <dit18142@go.uop.gr>
2021-04-26 18:44:14 +0300

    add logic to quiz

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,5 +1,4 @@
 var skip= document.getElementById('skip');
-console.log(skip)
 var score= document.getElementById('score');
 var totalScore= document.getElementById('totalscore');
 var timer= document.getElementById('timer');
@@ -11,7 +10,30 @@ var qa_quest = document.querySelectorAll('.qa_quest');
 var qa_answer = document.querySelectorAll('.qa_quest .qa_answer input');
 
 skip.addEventListener('click',function(){
-    step()
+    step();
+    duration=0
+    scoreCount-=15;
+    score.innerHTML=scoreCount;
+    totalScore.innerHTML = scoreCount;
+});
+
+qa_answer.forEach(function(qaAnswerSignle){
+    qaAnswerSignle.addEventListener('click',function(){
+        setTimeout(function(){
+            step();
+            duration=0;
+        },300);
+
+        var valid = this.getAttribute('valid');
+        if(valid == 'valid'){
+            scoreCount+=20;
+        }
+        else{
+            scoreCount-=15;
+        }
+        score.innerHTML=scoreCount;
+        totalScore.innerHTML = scoreCount;
+    })
 })
 
 function step(){
@@ -20,8 +42,24 @@ function step(){
         qa_quest[i].className='qa_quest';
     }
     qa_quest[count].className='qa_quest active';
-    if(count==5){
+    if(count==3){
         skip.style.display='none';
-
+        document.getElementById('qa_box').style.height='6em';
+        clearInterval(durationTime);
+        timer.style.display='none'
     }
 }
+
+var durationTime = setInterval(function(){
+    if(duration==0){
+        duration=10;
+    }
+    duration-=1;
+    timer.innerHTML=duration;
+    if(timer.innerHTML =="0"){
+        step();
+        scoreCount-=15;
+        score.innerHTML=scoreCount;
+        totalScore.innerHTML = scoreCount;
+    }
+},1000)
