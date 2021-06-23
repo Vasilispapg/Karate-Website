14792c354bc3c467ba6cf6cc27c18a3f0acefe7d
Polyxeni Motsi <polyxenhmotsi@outlook.com.gr>
2021-06-23 14:54:06 +0300

    emafnish quiz

--- scripts/questions.js
+++ scripts/questions.js
@@ -43,7 +43,7 @@ function createNewQuestion(q, id) {
     var type = q[7];
     //prwti apantisi
     //1 -> pollaplhs, 2->keimeno, 3->swsto/lathos
-    if (q[6] == 1 || q[6] == 3 || g[6] == 2) {
+    if (q[7] == 1 || q[7] == 3 || g[7] == 2) {
         var ans1 = createAnswers(q, id, 1); //ola ta eidh theloun thn prwth erwthsh
         div_quest.appendChild(ans1);
 
@@ -92,7 +92,7 @@ function createAnswers(q, id, count) {
     var input_radio = document.createElement("input");
     input_radio.type = 'radio';
     input_radio.name = 'q' + id;
-    if (q[6] == 1 || q[6] == 3) {
+    if (q[7] == 1 || q[7] == 3) {
         if (count == q[5]) //einai i swsti apantisi
             input_radio.setAttribute('valid', "valid");
         var span = document.createElement("span");
